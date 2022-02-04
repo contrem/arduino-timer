da7b82142965bd4cdbe33dbcbe57c4d26400e435
Greg <sixtelgreg@gmail.com>
2022-02-04 11:06:10 +0200

    Change left(),task_id() to const

--- src/arduino-timer.h
+++ src/arduino-timer.h
@@ -116,13 +116,13 @@ class Timer {
 
     /* Left until the task ends */
     unsigned long
-    left(const Task &task)
+    left(const Task &task) const
     {
         auto lft = 0ul;
         if (!task)
             return lft;
 
-        timer_foreach_task(t) {
+        timer_foreach_const_task(t) {
             if (t->handler && task_id(t) == task) {
                 const unsigned long now = time_func();
                 const unsigned long duration = now - t->start;
@@ -245,7 +245,7 @@ class Timer {
 
     inline
     Task
-    task_id(const struct task * const t)
+    task_id(const struct task * const t) const
     {
         const Task id = reinterpret_cast<Task>(t);
 
